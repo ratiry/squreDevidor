6d54d3d8263083d4015bd45e218f248c3d6c1014
unknown <ratiry@yandex.ru>
2023-08-26 11:28:49 +0300

    got rid of makeigquare

--- Test.js
+++ Test.js
@@ -1,36 +1,22 @@
 
 let findSquares=(n)=>{
     let d=2;
-    let squares={};
+    let squares=1;
     let i=0;
     while(d*d<=n){
         
         if(n%d==0){
             n/=d;
-            i+=1;
             if(n%d==0){
                 n/=d
-                i+=1;
-                squares[d]=i;
+                squares=squares*(d**2);
             }
         }else{
-            i=0;
             d+=1;
         }
 
     }
     return squares
 }
-let makeBigSquare=(obj)=>{
-    let square=1;
-    if(Object.keys(obj).length==0){
-        return 1
-    }
-    for(let i=0;i<Object.keys(obj).length;i++){
-        square*=Object.keys(obj)[i]**Object.values(obj)[i];
-    }
-    return square
-    }
-
-let squares=makeBigSquare(findSquares(900));
+let squares=findSquares(4536);
 console.log(squares)
